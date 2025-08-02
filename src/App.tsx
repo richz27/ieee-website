ce78a8285384d9384a0184badf2c79dab429bb06
Richard Zhang <richardzhangzzx@gmail.com>
2025-08-01 22:38:08 -0700

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import './App.css'
-import ieeelogo from './assets/images/logo-white.svg'
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <>
       <header>
-        {/* Logo */}
-        <img className="logo" src={ieeelogo}/>
-
-        {/* Top Navigation */}
-        <nav>
-          <a href="#home"> Home </a>
-          <a href="#about"> About Us </a>
-          <a href="#program"> Our Programs </a>
-          <a href="#team"> Team </a>
-        </nav>
-      </header>
+        <Navbar/> 
+      </header>     
 
       <main>
         <section id="hero">
           <p>Welcome to</p>
-          <h1>Northwestern IEEE Student Branch</h1>
+          <h1 className="font-bold underline">Northwestern IEEE Student Branch</h1>
         </section>
       </main>
  
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import { Menu, X } from "lucide-react"
+import { useState} from "react"; 
+import ieeelogo from '../assets/images/logo-white.svg'
+const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false); 
+
+    const toggleNavbar = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen); 
+    }
+
+
+    return (
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+            <div className="container px-4 mx-auto relative text-sm">
+                {/* Responsiveness: Hidden by default, only visible in large screen */}
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center flex-shrink-0">
+                        <img className="logo h-12 w-auto mr-2" src={ieeelogo} alt="IEEE Logo"/> 
+                    </div>
+                    <div className="hidden lg:flex mr-27 space-x-12">
+                        <a href="home">Home</a>
+                        <a href="about">About</a>
+                        <a href="program">Programs</a>
+                        <a href="Team">Team</a>
+                    </div>
+                    <div className="hidden lg:flex justify-center space-x-12 items center">
+                        <a href="#" className="bg-gradient-to-r from-sky-200 to-sky-500 py-2 px-3 rounded-md">
+                            Join Us!  
+                        </a>
+                    </div>
+                    <div className="lg:hidden md:flex flex-col justify-end">
+                        <button onClick={toggleNavbar}>
+                            {mobileDrawerOpen ? <X /> : <Menu />}
+                        </button>
+                    </div>
+                </div>
+                {mobileDrawerOpen && (
+                    <div className="className=fixed right-0 z-20 bg-violet-400 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                        <a className="py-4" href="home">Home</a>
+                        <a className="py-4" href="about">About</a>
+                        <a className="py-4" href="program">Programs</a>
+                        <a className="py-4" href="Team">Team</a>
+                    </div>
+                    // Todo: Join Us Button
+                )}
+            </div>
+        </nav>
+        
+    )
+}
+
+export default Navbar; 
